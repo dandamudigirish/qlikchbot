d0935734184dbd16cf1380455032c92746d5a415
dandamudigirish <31508792+dandamudigirish@users.noreply.github.com>
2017-08-31 12:42:57 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: e77d10e5-9aac-40c7-a5f4-b4437eeb78d2,
+    appPassword: c52HVV5Q6B4QJvd1dwRzmni
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
