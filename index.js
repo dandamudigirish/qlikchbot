bad9200e22089d593559814448c133885ada3cd2
dandamudigirish <31508792+dandamudigirish@users.noreply.github.com>
2017-08-31 17:00:31 -0700

    API AI test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var apiairecognizer = require('api-ai-recognizer');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -9,14 +10,35 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: e77d10e5-9aac-40c7-a5f4-b4437eeb78d2,
-    appPassword: c52HVV5Q6B4QJvd1dwRzmni
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users
-server.post('/api/messages', connector.listen());
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector);
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+//Initialize the API AI with the token
+var recognizer = new apiairecognizer(process.env.API_AI_TOKEN);
+var intents = new builder.IntentDialog({
+         recognizers: [recognizer]
+});
+
+// Receive messages from the user about the Business
+bot.dialog('/api/messages/',intents);
+
+intents.matches('SalesIntent',[
+    function(session,args){
+        var sales = builder.EntityRecognizer.findEntity(args.entities,'salesEntity');
+        if (sales){
+            var sales_name = sales.entity;
+            session.send("The"+sales_name+" is $122,431");
+        }else{
+            session.send("Hey ! I didn't get that...");
+        }
+    }
+]);
+
+intents.onDefault(function(session){
+    session.send("Sorry...can you please rephrase that?");
 });
